263975a197289b7bc9d21023c15a1d43b26b0fc8
Martin Alejandro Mammana <martin.ale590@gmail.com>
2023-11-08 20:28:24 -0300

    implemented create a post

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -3,8 +3,9 @@ import {
     useMutation,
     useQueryClient,
     useInfiniteQuery } from '@tanstack/react-query'
-import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
-import { INewUser } from '@/types'
+import { createPost, createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
+import { INewPost, INewUser } from '@/types'
+import { QUERY_KEYS } from './queries'
 
 export const useCreateUserAccount = () => {
     return useMutation({
@@ -21,5 +22,17 @@ export const useSignInAccount = () => {
 export const useSignOutAccount = () => {
     return useMutation({
         mutationFn: signOutAccount
+    })
+}
+
+export const useCreatePost = () => {
+    const queryClient = useQueryClient()
+    return useMutation({
+        mutationFn: (post: INewPost) => createPost(post),
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+            })
+        }
     })
 }
